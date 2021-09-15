a5770d8dba8e3fdecaf82b982289b224a6ad33f6
Cozmo <mohamed@lenovo.local>
2021-09-15 02:36:03 +0200

    removed bootstrap css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignPage from "./pages/SignPage";
 import Navbar from "./components/Navbar";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
